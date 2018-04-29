8a5aa0b8e2492572c8f23b524c85ce82df7466ec
Andrew Bailey <alb@andrewbailey.me>
2018-04-29 10:53:10 -0400

    Added sass styling structure and breakpoint styling

--- src/App.vue
+++ src/App.vue
@@ -10,31 +10,4 @@ export default {
 }
 </script>
 
-<style>
-body, html {
-  background-color: rgb(192, 0, 0); /* chucknorris */
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 100%;
-}
-
-h1, h2, h3, h4 {
-  margin: 0;
-}
-
-p {
-  margin: 0;
-  padding: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  height: 100%;
-  width: 100%;
-  font-size: 14px;
-}
-</style>
+<style></style>
--- src/components/ChuckPunch.vue
+++ src/components/ChuckPunch.vue
@@ -3,14 +3,14 @@
     <div class="punch" v-show="isLoading"></div>
     <div class="chuck-punch__body">
       <div class="chuck-punch__quote-box" v-show="!isLoading">
-        <p><span class="quote-mark"></span>{{joke}}</p>
+        <p class="chuck-punch__quote"><span class="quote-mark"></span>{{joke}}</p>
         <div class="chuck-punch__btn" @click="getPunchedRandomly()">
           Hit me Chuck
           <span class="chuck-punch__chuck-face"></span>
         </div>
       </div>
       <audio preload="auto">
-        <source src="../assets/punch-sound.mp3" type="audio/mpeg" />
+        <source src="../assets/img/punch-sound.mp3" type="audio/mpeg" />
         This text displays if the audio tag isn't supported.
       </audio>
     </div>
@@ -59,71 +59,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-  .chuck-punch {
-    height: 100%;
-    display: flex;
-    flex-flow: column wrap;
-  }
-
-  .punch {
-    position: absolute;
-    background: url('../assets/punch.svg') center center no-repeat;
-    background-size: contain;
-    height: 100%;
-    width: 100%;
-  }
-
-  .chuck-punch__body {
-    flex: 1;
-    display: flex;
-    flex-flow: column nowrap;
-    align-items: center;
-    justify-content: center;
-    padding: 3em;
-  }
-
-  .chuck-punch__quote-box {
-    position: relative;
-    padding-left: 4.5em;
-  }
-
-  p {
-    max-width: 800px;
-    font-size: 3.5em;
-    font-weight: bold;
-    color: white;
-    margin-bottom: 1em;
-  }
-
-  .quote-mark {
-    position: absolute;
-    top: -.5em;
-    left: 0;
-    background: url('../assets/quotation.svg') center no-repeat;
-    height: 1.15em;
-    width: 1.15em;
-  }
-
-  .chuck-punch__btn {
-    position: relative;
-    display: inline-block;
-    font-size: 2em;
-    border: 2px solid #fff;
-    padding: .5em 2.25em .5em 1em;
-    color: #fff;
-  }
-
-  .chuck-punch__chuck-face {
-    position: absolute;
-    top: 50%;
-    left: 0;
-    transform: translateY(-50%);
-    background: url('../assets/chuck-norris-sticker.png') 92% no-repeat;
-    background-size: contain;
-    height: 75%;
-    width: 100%;
-  }
-
-</style>
+<style scoped></style>
